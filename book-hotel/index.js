29849971cb046d71d5a9066f2c371189dd19076f
Sami <52921619+sbagh@users.noreply.github.com>
2022-12-09 10:11:58 -0500

    Project Setup

---
+++ book-hotel/index.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const fetch = require('node-fetch');
+
+let url = "https://xvot5ogu90.execute-api.us-east-1.amazonaws.com/Prod  
+
+exports.handler = async (evt) => {
+
+    .POST()
+
+    post 
+
+
+    return var
+}
